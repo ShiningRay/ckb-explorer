37beb2d44f9605d7cd3d4ed6d3c3eee4d9253eec
ShiningRay <tsowly@hotmail.com>
2023-02-13 11:09:25 +0000

    optimize github action

--- app/workers/update_block_size_worker.rb
+++ app/workers/update_block_size_worker.rb
@@ -2,10 +2,10 @@ class UpdateBlockSizeWorker
   include Sidekiq::Worker
   sidekiq_options queue: "low"
 
-  def perform(block_id)
+  def perform(block_id, force=false)
     block = Block.find_by(id: block_id)
     return if block.blank?
-
+    return if block.block_size? && !force
     node_block = CkbSync::Api.instance.get_block_by_number(block.number)
     block.update(block_size: node_block.serialized_size_without_uncle_proposals)
   end
--- app/workers/update_tx_bytes_worker.rb
+++ app/workers/update_tx_bytes_worker.rb
@@ -2,8 +2,9 @@ class UpdateTxBytesWorker
   include Sidekiq::Worker
   sidekiq_options queue: "low"
 
-  def perform(tx_id)
+  def perform(tx_id, force=false)
     tx = CkbTransaction.find tx_id
+    return if tx.bytes? && !force
     res = CkbSync::Api.instance.directly_single_call_rpc(method: :get_transaction, params: [tx.tx_hash, "0x0"])
     tx.bytes = (res["result"]["transaction"].size - 2) / 2 + 4
     tx.save
